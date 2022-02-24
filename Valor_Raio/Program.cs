740283e78bc8e849bd26176c63785f9d6420df7e
Fabio2222 <fabiomoreira73@outlook.com>
2022-02-24 10:54:38 -0300

    Add project files.

---
+++ Valor_Raio/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Valor_Raio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi=3.1416, raio;
+
+            Console.WriteLine("Escreva o raio");
+            raio = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("A area do circulo e {0}", (pi*raio*raio));
+            
+
+            
+            Console.ReadLine();
+        }
+    }
+}
